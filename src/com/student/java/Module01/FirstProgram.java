12c64fe44bb10c47dbe4665ffca73c9816d15a30
Seliutina <iryna.korotych@gmail.com>
2016-05-26 02:17:24 +0300

    Initial commit of the project.y

---
+++ src/com/student/java/Module01/FirstProgram.java
@@ -0,0 +1,7 @@
+package com.student.java.Module01;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi! I'm your first Java program! Stay tuned!");
+    }
+}
