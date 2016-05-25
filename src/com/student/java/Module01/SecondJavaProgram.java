e01c0f89fd8d949ec8af319208f0a375b0b8de15
Seliutina <iryna.korotych@gmail.com>
2016-05-26 02:34:57 +0300

    Edding SecondJavaProgram.class.

---
+++ src/com/student/java/Module01/SecondJavaProgram.java
@@ -0,0 +1,11 @@
+package com.student.java.Module01;
+
+/**
+ * Created by Irina on 26.05.2016.
+ */
+public class SecondJavaProgram
+{
+    public static void main(String[] args) {
+        System.out.println("This is second program");
+    }
+}
